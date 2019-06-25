b835de416da89d1bca351b13abf21b4b489f7d53
Xinhe <xinhe998@gmail.com>
2019-06-26 01:49:20 +0800

    add api url config

---
+++ config/api.js
@@ -0,0 +1,9 @@
+const API_URL = require('./project_config').api.url;
+
+module.exports = {
+  register: `${API_URL}/register/register`,
+  login: `${API_URL}/login/login`,
+  create_project: `${API_URL}/member/CRPj`,
+  create_file: `${API_URL}/member/CRjDetail`,
+  update_file: `${API_URL}/member/UPPjDetail`,
+};
