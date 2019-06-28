1dd85cbe834ebd2c04525dd1faf0fe0232720d8b
Xinhe <xinhe998@gmail.com>
2019-06-28 15:55:41 +0800

    add UserInfo component

---
+++ src/components/AppHeader/UserInfo/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './index.scss';
+
+class UserInfo extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+UserInfo.propTypes = {
+};
+
+UserInfo.defaultProps = {
+};
+export default UserInfo;
