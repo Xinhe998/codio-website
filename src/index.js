5eb6ff6238f430fb2cba424503bd712216d6c354
John.Yang <stu60610@gmail.com>
2019-09-13 22:05:20 +0800

    Fix Modal routing issue

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
@@ -11,6 +12,7 @@ import App from './pages';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ForgetPassword from './pages/ForgetPassword';
+import usePrevious from './hooks/usePrevious';
 
 const store = createStore(
   storeFs,
@@ -22,16 +24,30 @@ const store = createStore(
   ),
 );
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <Switch>
+const CodioSwitch = ({ location }) => {
+  const previousLocation = usePrevious(location);
+  const isModal = !!(location.state && location.state.modal);
+  return (
+    <>
+      <Switch location={isModal ? previousLocation : location}>
         <Route exact path="/" component={App} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
-        <Route path="/forget_password" component={ForgetPassword} />
         {/* <Route path="/:type(foods|drinks)" component={App} /> */}
       </Switch>
+      <Route path="/forget_password" component={ForgetPassword} />
+    </>
+  );
+};
+
+CodioSwitch.propTypes = {
+  location: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Route component={CodioSwitch} />
     </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
