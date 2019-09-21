573965c7c783dc761b512c7e47b3135f8da5cd88
Xinhe <xinhe998@gmail.com>
2019-09-21 21:07:33 +0800

    change TextInput to functional component

--- src/components/TextInput/index.js
+++ src/components/TextInput/index.js
@@ -1,59 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { IoIosWarning, IoIosCheckmarkCircle } from 'react-icons/io';
 import './index.scss';
 
-class TextInput extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  render() {
-    const {
-      type,
-      placeholder,
-      text,
-      title,
-      onChange,
-      required,
-      showHint,
-      hintType,
-      hintText,
-      onFocus,
-      onBlur,
-      icon,
-    } = this.props;
-    return (
-      <div
-        className={classNames(
-          'textinput',
-          required ? 'required' : null,
-          showHint ? hintType || null : null,
-        )}
-      >
-        <span className="textinput__title">{title}</span>
-        <input
-          type={type}
-          value={text}
-          placeholder={placeholder}
-          onChange={e => onChange(e)}
-          onFocus={onFocus}
-          onBlur={onBlur}
-        />
-        <div className="textinput__icon">{icon}</div>
-        {showHint ? (
-          <span className="textinput__hint">
-            <span className="hinttext">{hintText}</span>
-            {hintType === 'error' ? <IoIosWarning /> : null}
-            {hintType === 'ok' ? <IoIosCheckmarkCircle /> : null}
-          </span>
-        ) : null}
-      </div>
-    );
-  }
-}
+const TextInput = ({
+  type,
+  placeholder,
+  text,
+  title,
+  onChange,
+  required,
+  showHint,
+  hintType,
+  hintText,
+  onFocus,
+  onBlur,
+  icon,
+}) => {
+  return (
+    <div
+      className={classNames(
+        'textinput',
+        required ? 'required' : null,
+        showHint ? hintType || null : null,
+      )}
+    >
+      <span className="textinput__title">{title}</span>
+      <input
+        type={type}
+        value={text}
+        placeholder={placeholder}
+        onChange={e => onChange(e)}
+        onFocus={onFocus}
+        onBlur={onBlur}
+      />
+      <div className="textinput__icon">{icon}</div>
+      {showHint ? (
+        <span className="textinput__hint">
+          <span className="hinttext">{hintText}</span>
+          {hintType === 'error' ? <IoIosWarning /> : null}
+          {hintType === 'ok' ? <IoIosCheckmarkCircle /> : null}
+        </span>
+      ) : null}
+    </div>
+  );
+};
 
 TextInput.propTypes = {
   title: PropTypes.string,
