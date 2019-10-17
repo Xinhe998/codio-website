0bb06eb0285f4f0c0c706fd8e9d497bbdc15b951
Xinhe <xinhe998@gmail.com>
2019-10-17 16:53:31 +0800

    update ci config

--- config/api.js
+++ config/api.js
@@ -1,4 +1,4 @@
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production' || process.env.CIRCLECI) {
   var { API_URL } = process.env;
 } else {
   var API_URL = require('./project_config').api.url;
