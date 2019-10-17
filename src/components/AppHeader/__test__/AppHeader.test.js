492d54b367d224993b77a44a800073d373c9451e
Xinhe <xinhe998@gmail.com>
2019-10-17 19:07:11 +0800

    test

--- src/components/AppHeader/__test__/AppHeader.test.js
+++ src/components/AppHeader/__test__/AppHeader.test.js
@@ -54,7 +54,7 @@ describe('AppHeader', function() {
       expect(wrapper).toMatchSnapshot();
     });
     it('should find div', async () => {
-      expect(testComp.find('.AppHeader123'));
+      expect(testComp.find('.123'));
     });
     // 測試icon
     // it('should render icon', () => {
