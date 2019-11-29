f131cbc15a0eb45c23036d5887d2606d0d37e488
Xinhe <xinhe998@gmail.com>
2019-11-30 01:28:44 +0800

    connect save portfolio & get portfolio API

--- src/reducers/portfolioReducer.js
+++ src/reducers/portfolioReducer.js
@@ -1,16 +1,35 @@
-const portfolioInitialState = [];
+const portfolioInitialState = {
+  contents: '',
+  m_no: '',
+  mp_desc: '',
+  mp_func: '',
+  mp_hashtag: '',
+  mp_isPublic: '',
+  mp_name: '',
+  mp_no: '',
+};
 
-export default function (state = portfolioInitialState, action) {
+export default function(state = portfolioInitialState, action) {
   switch (action.type) {
-  case 'GET_PORTFOLIO': {
-    const { status, data } = action.res;
-    return status === 200 ? data.data : state;
-  }
-  case 'CREATE_PORTFOLIO': {
-    const { status, data } = action.res;
-    return status === 200 ? data.data : state;
-  }
-  default:
-    return state;
+    case 'GET_PORTFOLIO': {
+      const { status, data } = action.res;
+      const portfolio = {
+        contents: data.data,
+        m_no: data.datatitle.mp_no, // 作者
+        mp_desc: data.datatitle.mp_desc,
+        mp_func: data.datatitle.mp_func,
+        mp_hashtag: data.datatitle.mp_hashtag,
+        mp_isPublic: data.datatitle.mp_isPublic,
+        mp_name: data.datatitle.mp_name,
+        mp_no: data.datatitle.mp_no,
+      };
+      return status === 200 ? portfolio : state;
+    }
+    case 'CREATE_PORTFOLIO': {
+      const { status, data } = action.res;
+      return status === 200 ? data.data : state;
+    }
+    default:
+      return state;
   }
 }
